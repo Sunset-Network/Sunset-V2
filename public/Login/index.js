06bf1a90706627d9658013303123edb01fe57193
Nobodycares-lo <140128868+Nobodycares-lo@users.noreply.github.com>
2024-08-27 19:55:40 -0600

    meow

--- public/Login/index.js
+++ public/Login/index.js
@@ -1,5 +1,5 @@
-let username = "dingus"; // edit txt in "" to change username
-let password = "iscute"; // edit txt in "" to change password
+let username = "sunset"; // edit txt in "" to change username
+let password = "test"; // edit txt in "" to change password
 
 document.getElementById("submitbtn").onclick = function(event){
     event.preventDefault(); // Prevent the default form submission action
